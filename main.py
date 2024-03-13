acd852543dfcaeb07fc6515749d818fd2c1385a3
Żabek <52965072+Zabeek@users.noreply.github.com>
2024-03-13 12:15:48 +0100

    Przyspieszenie działania programu
    ulepszona wersja programu

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import random
 import numpy as np
 
 
@@ -13,45 +12,28 @@ def oblicz_wygrana_hordy(wygrane):
     return np.max(wygrana_hordy)
 
 
-def symulacja_kasyna(N, M, f_prawdziwy, Tmax):
-    # Inicjalizacja parametrów a_ij i b_ij dla każdego gościa i maszyny
+def symulacja_kasyna(N, M, f_prawdziwy, Tmax, krok_wyswietlania=1000):
     a = np.ones((M, N))
     b = np.ones((M, N))
-    # Inicjalizacja listy przechowującej wygrane
     wygrane = np.zeros((M, N))
 
-    # Pętla symulująca gry dla każdego kroku czasowego
     for t in range(Tmax):
-        # Symulacja gry dla jednorękich bandytów
-        for i in range(N):
-            for j in range(M):
-                # Generowanie wygranej zgodnie z rozkładem f_prawdziwy[i]
-                wygrana = np.random.normal(f_prawdziwy[i], 1)
-                # Aktualizacja parametrów a_ij i b_ij dla każdego gościa i maszyny
-                a[j][i], b[j][i] = aktualizacja_parametrow(a[j][i], b[j][i], wygrana)
-                # Dodanie wygranej do listy wygranych
-                wygrane[j][i] = wygrana
+        wygrane_losowe = np.random.normal(f_prawdziwy, 1, size=(M, N))
+        wygrane += wygrane_losowe
 
-        # Wyświetlanie wyników co pewną liczbę kroków czasowych
-        if t % 1000 == 0:
+        a, b = aktualizacja_parametrow(a, b, wygrane_losowe)
+
+        if t % krok_wyswietlania == 0:
             print(f"Krok czasowy: {t}")
-            for j in range(M):
-                print(f"Wyniki dla gościa {j + 1}:")
-                print(wygrane[j])
-                print()
-
-            # Obliczenie i wyświetlenie maksymalnej wygranej hordy
             max_wygrana_hordy = oblicz_wygrana_hordy(wygrane)
             print(f"Maksymalna wygrana hordy: {max_wygrana_hordy}\n")
 
     return wygrane
 
 
-# Parametry symulacji
 N = 100  # liczba jednorękich bandytów
 M = 50  # liczba gości
 Tmax = 10000  # liczba kroków czasowych
-f_prawdziwy = [random.uniform(0, 1) for _ in range(N)]  # rozkład prawdopodobieństwa wygranej dla każdego bandyty
+f_prawdziwy = np.random.uniform(0, 1, size=N)  # rozkład prawdopodobieństwa wygranej dla każdego bandyty
 
-# Wywołanie funkcji symulującej kasyno
 wyniki_symulacji = symulacja_kasyna(N, M, f_prawdziwy, Tmax)
